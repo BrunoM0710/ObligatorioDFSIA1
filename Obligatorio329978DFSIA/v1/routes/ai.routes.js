07ce5cfc755ef2e66cb1f011b1c9a124bef1b6c4
BrunoM0701 <bm329978@fi365.ort.edu.uy>
2026-05-14 15:31:27 -0300

    fix rapido IA

--- Obligatorio329978DFSIA/v1/routes/ai.routes.js
+++ Obligatorio329978DFSIA/v1/routes/ai.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getModels, useGemini25Flash } from "../controllers/ai.controllers";
+import { getModels, useGemini25Flash } from "../controllers/ai.controllers.js";
 
 const router = express.Router({ mergeParams: true });
 
