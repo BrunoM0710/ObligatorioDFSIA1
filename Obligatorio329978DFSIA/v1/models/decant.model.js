2486b7e3215913ce46a1d9279b35d59d08c75613
BrunoM0701 <bm329978@fi365.ort.edu.uy>
2026-06-10 07:49:14 -0300

    Update decant.model.js

--- Obligatorio329978DFSIA/v1/models/decant.model.js
+++ Obligatorio329978DFSIA/v1/models/decant.model.js
@@ -5,13 +5,29 @@ const decantSchema = new mongoose.Schema({
   usuario: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Usuario",
-    required: true
+    required: true,
   },
   perfume: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Perfume"
-  }
+    ref: "Perfume",
+  },
+  perfumeNombre: {
+    type: String,
+    required: true,
+  },
+  perfumeMarca: {
+    type: String,
+    required: true,
+  },
+  perfumeImagen: {
+    type: String,
+    required: true,
+  },
+  perfumeConcentracion: {
+    type: String,
+    required: true,
+  },
 });
- 
+
 const Decant = mongoose.model("Decant", decantSchema);
 export default Decant;
