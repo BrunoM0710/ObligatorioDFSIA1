f0df5fbe3de587fe1f610e958cfc34c134294bf4
BrunoM0701 <bm329978@fi365.ort.edu.uy>
2026-06-10 12:22:07 -0300

    Update v1.routes.js

--- Obligatorio329978DFSIA/v1/v1.routes.js
+++ Obligatorio329978DFSIA/v1/v1.routes.js
@@ -13,16 +13,13 @@ const router = express.Router({ mergeParams: true });
 //rutas de login y registro de usuarios, sin token
 
 router.use("/auth", authRouter);
-router.get("/perfumes", perfumesRouter);
-router.get("/perfumes:id", perfumesRouter);
-
+router.use("/perfumes", perfumesRouter);
 
 //middleware para verificar tokens
 router.use(authenticateMiddleware);
 
 //acceso endpoints protegidos
-router.post("/perfumes", perfumesRouter);
-router.delete("/perfumes", perfumesRouter);
+
 router.use("/ordenes", ordenesRouter);
 router.use("/usuarios", usuariosRouter);
 router.use("/decant", decantRouter);
